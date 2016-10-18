de04be478b180c1f070d7f9877169720b348d35b
zy-xx <xx198742>
2016-10-19 07:53:51 +0800

    测试提交

--- gxwj/src/main/java/cn/sinobest/policeunion/biz/gxwj/graph/core/Cycle.java
+++ gxwj/src/main/java/cn/sinobest/policeunion/biz/gxwj/graph/core/Cycle.java
@@ -16,28 +16,37 @@ public class Cycle {
     public Cycle(Graph graph) {
         Set<GraphNode> nodes = graph.getNodes();
         for (GraphNode node:nodes){
-            if (!maked.get(node)){
+            if (maked.get(node) == null || !maked.get(node)){
                 dfs(graph, node, node);
+            }
+        }
+        System.out.println("end");
+    }
+
+    private void dfs(Graph graph,GraphNode node,GraphNode lastNode){
+        maked.put(node, true);
+        for (GraphNode toNode:graph.adj(node)){
+            if (maked.get(toNode) == null || !maked.get(toNode)){
+                edgeTo.put(toNode,node);
+                dfs(graph, toNode, node);
+//                edgeTo.remove(toNode);
+//            }else if (!toNode.equals(lastNode)){
+//                Stack<GraphNode> cycle = new Stack<GraphNode>();
+//                for (GraphNode x=node;x!=toNode;x=edgeTo.get(x)){
+//                    cycle.push(x);
+//                }
+//                cycle.push(toNode);
+//                cycle.push(node);
+//                cycles.add(cycle);
             }
         }
     }
 
-    public void dfs(Graph graph,GraphNode node,GraphNode lastNode){
-        maked.put(node, true);
-        for (GraphNode toNode:graph.adj(node)){
-            if (!maked.get(toNode)){
-                edgeTo.put(toNode,node);
-                dfs(graph, toNode, node);
-                edgeTo.remove(toNode);
-            }else if (!toNode.equals(lastNode)){
-                Stack<GraphNode> cycle = new Stack<GraphNode>();
-                for (GraphNode x=node;x!=toNode;x=edgeTo.get(x)){
-                    cycle.push(x);
-                }
-                cycle.push(toNode);
-                cycle.push(node);
-                cycles.add(cycle);
-            }
-        }
+    public boolean hasCycles(){
+        return cycles.isEmpty();
+    }
+
+    public List<Stack<GraphNode>> getCycles() {
+        return cycles;
     }
 }
