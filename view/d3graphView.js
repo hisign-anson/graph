206161a8fedd03c8460d765be39fbc961c4a6ae4
miya <mayan_0514@163.com>
2018-07-27 17:29:50 +0800

    删除文件

--- view/d3graphView.js
+++
@@ -1,7 +0,0 @@
-
-require(['../plugins/require/require_config.js','../js/graphLayout'], function (config, graphLayout){
-    var jsonInitUrl = "../json_data/huangshijinTest.json";
-    graphLayout.updateGraphURL(jsonInitUrl);
-    graphLayout.clickEvent();
-
-});
